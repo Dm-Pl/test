e47d11d4e87ff3c8835f12aeb37fd4de4fba6c42
DmPol <polis.dm@gmail.com>
2018-01-16 16:08:29 +0200

    Test version

---
+++ test_soup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3.5
+import urllib
+from bs4 import BeautifulSoup
+
+quote_page = 'http://finance.ua/ua/currency'
+
+page = urllib.request.urlopen(quote_page)
+
+soup = BeautifulSoup(page, 'html.parser')
+
+name_box = soup.find('td', attrs={'class': 'c3'})
+
+# print(list(soup.children))
+
+print(name_box)
